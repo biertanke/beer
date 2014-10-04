530a01d79fd91eb757b8145aeb26ef5708288822
Daniel Greuter <daniel@Daniels-MacBook-Air.local>
2014-10-04 12:15:19 +0200

    hmmmm

--- simplejava.java
+++ simplejava.java
@@ -1,6 +1,38 @@
-class simplejava {
-	
+/*
+ * Beispielprogramm 1
+ * Copyright by biertanke
+*/
+
+/**
+ * Gibt nur einen Text aus
+ *
+ *
+ * @author biertanke
+ * @version 1.0
+ * @see java.lang.System
+ */
+
+
+import javax.swing.JFrame;
+
+public class simplejava {
+	/**
+	* main method
+	* @param args (siehe spaeter)
+	*/
 	public static void main(String args[]) {
-		System.out.println("You'll love java!");
+		JFrame myFrame = new JFrame();
+		String myTitle = "Blank Frame";
+
+		myFrame.setTitle(myTitle);
+		myFrame.setSize(300,200);
+		myFrame.setDefaultCloseOperation
+			(JFrame.EXIT_ON_CLOSE);
+		myFrame.setVisible(true);
+
+
+		int amountOfMoney;
+		amountOfMoney = 50;
+		System.out.println(amountOfMoney);
 	}
 }
